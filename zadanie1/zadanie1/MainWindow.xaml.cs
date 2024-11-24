89f11f5623aab7823e30f166600cbc0f1c672afe
Minciak-z <nikolanowak2007n@gmail.com>
2024-11-24 16:34:35 +0100

    Add files via upload

---
+++ zadanie1/zadanie1/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace Pensje
+{
+    public partial class Kalkulator : Window
+    {
+        public Kalkulator()
+        {
+            InitializeComponent();
+        }
+
+        private void Oblicz(object sender, RoutedEventArgs e)
+        {
+            if (decimal.TryParse(txtBrutto.Text, out decimal brutto) && brutto > 0)
+            {
+                decimal netto = brutto - brutto * 0.12m;
+
+                listBrutto.Items.Add(brutto.ToString("C"));
+                listNetto.Items.Add(netto.ToString("C"));
+
+                txtBrutto.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Wprowadź poprawną wartość  brutto.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ zadanie2/zadanie2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+
+namespace Pensja
+{
+    public partial class MainWindow : Window
+    {
+        private decimal pensja = 0.12m; 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private decimal ObliczPensje(decimal brutto)
+        {
+            return brutto - brutto * pensja;  
+        }
+
+        private void Oblicz(object sender, RoutedEventArgs e)
+        {
+            if (decimal.TryParse(txtBrutto.Text, out decimal brutto) && brutto > 0)
+            {
+                decimal netto = ObliczPensje(brutto);
+
+                listBrutto.Items.Add(brutto.ToString("C"));
+                listNetto.Items.Add(netto.ToString("C"));
+
+                txtBrutto.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Wprowadź poprawną wartość brutto.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Check(object sender, RoutedEventArgs e)
+        {
+            if (radioButton8_5.IsChecked == true)
+                pensja = 0.085m;
+            else if (radioButton12.IsChecked == true)
+                pensja = 0.12m;
+            else if (radioButton19.IsChecked == true)
+                pensja = 0.19m;
+        }
+    }
+}
